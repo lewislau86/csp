92a0373c91c00fe14535baec5800da5ed0e8f5b4
lewislau <lewislau86@gmail.com>
2019-05-23 17:10:09 +0800

    Add Google Test for test framework

--- csp/CSPEncrypt.cpp
+++ csp/CSPEncrypt.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <windows.h>
 #include <wincrypt.h>
+#include <stdio.h>
 #include "CSPEncrypt.h"
+
+//////////////////////////////////////////////////////////////////////////
+//
 CSPEncrypt::CSPEncrypt()
 {
 	DWORD dwStatus = 0;
 	m_hProv = NULL;
-    wchar_t info[] = L"Microsoft Enhanced RSA and AES Cryptographic Provider";
+    WCHAR info[] = L"Microsoft Enhanced RSA and AES Cryptographic Provider";
 	if (!CryptAcquireContext(&m_hProv, NULL, info, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) 
 	{
 		dwStatus = GetLastError();
@@ -16,8 +20,66 @@ CSPEncrypt::CSPEncrypt()
 	}
 }
 
+//////////////////////////////////////////////////////////////////////////
+//
 CSPEncrypt::~CSPEncrypt()
 {
     if (NULL != m_hProv && FALSE == CryptReleaseContext(m_hProv, 0))
         OutputDebugString(TEXT("Construction Error!\r\n"));
 }
+
+//////////////////////////////////////////////////////////////////////////
+//
+BYTE* CSPEncrypt::AseEncrypt()
+{
+    return NULL;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+BYTE* CSPEncrypt::AesDecrypt()
+{
+    return NULL;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+BYTE* CSPEncrypt::RsaEncrypt()
+{
+    return NULL;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+BYTE* CSPEncrypt::RsaDecrypt()
+{
+    return NULL;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+LPVOID CSPEncrypt::Base64Encode(LPVOID in, DWORD inLen, DWORD flags)
+{
+    DWORD  outLen;
+    LPVOID out = NULL;
+
+    // calculate space for string
+    if (CryptBinaryToString(in, inLen, CRYPT_STRING_BASE64 | flags, NULL, &outLen)) {
+        out = malloc(outLen);
+
+        // convert it
+        if (out != NULL) {
+            CryptBinaryToString(in, inLen,
+                CRYPT_STRING_BASE64 | flags, out, &outLen);
+        }
+    }
+    return out;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+
+
+
+// EOF
+//////////////////////////////////////////////////////////////////////////
--- csp/CSPEncrypt.h
+++ csp/CSPEncrypt.h
@@ -5,6 +5,14 @@ class CSPEncrypt
 public:
 	CSPEncrypt();
 	~CSPEncrypt();
+    // ASE
+    BYTE*   AseEncrypt();
+    BYTE*   AesDecrypt();
+    
+    BYTE*   RsaEncrypt();
+    BYTE*   RsaDecrypt();
+    LPVOID   Base64Encode(LPVOID in, DWORD inLen, DWORD flags);
+    DWORD   Base64Decode();
 
 private:
 	HCRYPTPROV m_hProv;
